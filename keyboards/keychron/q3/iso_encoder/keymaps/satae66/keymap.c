5e3b7407000d5ffcb00a06ca2bbdf214e48d7c8f
Mathias Boegl <mathias.boegl@gmail.com>
2025-03-01 13:12:07 +0100

    Support for german umlaute äöüß

--- keyboards/keychron/q3/iso_encoder/keymaps/satae66/keymap.c
+++ keyboards/keychron/q3/iso_encoder/keymaps/satae66/keymap.c
@@ -23,6 +23,26 @@ enum layers{
     WIN_FN
 };
 
+enum unicode_names {
+    AE_LOWER,
+    AE_UPPER,
+    OE_LOWER,
+    OE_UPPER,
+    UE_LOWER,
+    UE_UPPER,
+    SS
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [AE_LOWER] = 0x00E4, // ä
+    [AE_UPPER] = 0x00C4, // Ä
+    [OE_LOWER] = 0x00F6, // ö
+    [OE_UPPER] = 0x00D6, // Ö
+    [UE_LOWER] = 0x00FC, // ü
+    [UE_UPPER] = 0x00DC, // Ü
+    [SS]       = 0x00DF, // ß
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_f13_iso(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RM_VALD,  RM_VALU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,  KC_NO,    KC_NO,    RM_NEXT,
@@ -45,16 +65,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,              KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,    KC_ENT,
-        KC_LSFT,  XXXXXXX,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
+        KC_LSFT,  UC_LINX,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,            KC_UP,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_tkl_f13_iso(
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  KC_MRWD,  _______,  KC_MFFD,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
-        RM_TOGG,  RM_NEXT,  RM_VALU,  RM_HUEU,  RM_SATU,  RM_SPDU,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,  _______,
-        _______,  RM_PREV,  RM_VALD,  RM_HUED,  RM_SATD,  RM_SPDD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,  _______,  _______,  _______),
+        RM_TOGG,                 RM_NEXT,  RM_PREV,  RM_SPDU,  RM_SPDD,  RM_SATU,  RM_SATD,                 _______,  _______,                 _______,  _______,  _______,  _______,    _______,  KC_MRWD,  _______,  KC_MFFD,
+        _______,                 _______,  _______,  _______,  _______,  _______,  _______,                 _______,  _______,                 _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,
+        _______,                 _______,  _______,  _______,  _______,  _______,  _______,  UP(UE_LOWER, UE_UPPER),  _______,  UP(OE_LOWER, OE_UPPER),  _______,  _______,  _______,              _______,  _______,  _______,
+        _______,  UP(AE_LOWER, AE_UPPER),   UM(SS),  _______,  _______,  _______,  _______,                 _______,  _______,                 _______,  _______,  _______,  _______,    _______,
+        _______,                  UC_WIN,  _______,  _______,  _______,  _______,  _______,                 _______,  _______,                 _______,  _______,  _______,              _______,            _______,
+        _______,                 _______,  _______,                                _______,                                                              _______,  _______,  _______,    _______,  _______,  _______,  _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
